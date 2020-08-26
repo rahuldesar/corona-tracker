3a0ec71ac0a8d5b53d2c2d5ecb99a740e19ab8a5
Rahul Desar <desarrahul@gmail.com>
2020-08-26 13:44:07 +0545

    Updates

---
+++ precache-manifest.cf412f9887dfc05bdd6d9832ec769516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ea277598bc772b81d685279a2744e8",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "522491e7efe56b7786e3",
+    "url": "/corona-tracker/static/css/main.7e1f91f8.chunk.css"
+  },
+  {
+    "revision": "94fb1ff281f8b520e52b",
+    "url": "/corona-tracker/static/js/2.4d32df66.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-tracker/static/js/2.4d32df66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522491e7efe56b7786e3",
+    "url": "/corona-tracker/static/js/main.baab863f.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  }
+]);
